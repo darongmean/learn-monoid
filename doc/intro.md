e281ff40d2e2ef19a1dc6809236cedd4134ce877
Darong Mean <darongmean@users.noreply.github.com>
2020-08-19 21:38:56 +0700

    have fun

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learn-monoid
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/darong/learn_monoid_test.clj
@@ -0,0 +1,7 @@
+(ns com.darong.learn-monoid-test
+  (:require [clojure.test :refer :all]
+            [com.darong.learn-monoid :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
